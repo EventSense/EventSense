3cab7abb86365f8ae33d5b4484c2a7bb180c5482
Drew Cuthbertson <syeoryn@gmail.com>
2014-05-28 02:12:56 -0700

    Comments out code for testing

--- server/helpers.js
+++ server/helpers.js
@@ -1,12 +1,13 @@
 var request = require('request');
 
 exports.sendMessage = function(message, number){
-  var text = {
-    from: 'EventSense',
-    to: number,
-    text: message,
-  }
-  request.post('https://rest.nexmo.com/sms/json',{
-    body: text
-  })
+  // var text = {
+  //   from: 'EventSense',
+  //   to: number,
+  //   text: message,
+  // };
+  
+  // request.post('https://rest.nexmo.com/sms/json',{
+  //   body: text
+  // });
 }
